d27c722fa2c73e69e6184429ea80e6f0b00269dd
Byron-Ben <bbenp@miumg.edu.gt>
2025-10-03 16:55:58 -0600

    Se agrega clase de postulantes en servicios

---
+++ ReclutadoraProject/Reclutadora/src/main/java/programaciondos/reclutadora/Application/Services/PostulantesService.java
@@ -0,0 +1,14 @@
+package programaciondos.reclutadora.Application.Services;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author biron
+ */
+public class PostulantesService {
+    
+}
