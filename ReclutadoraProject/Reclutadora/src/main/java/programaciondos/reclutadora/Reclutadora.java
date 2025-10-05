8e5fe185abdd3441cbf4aa5d555cf8c2ff914d53
Estiben-Hernandez <jhernandezg92@miumg.edu.gt>
2025-10-04 18:21:00 -0600

    feat: Consumir el menú principal en el metodo main (Release de prueba)

--- ReclutadoraProject/Reclutadora/src/main/java/programaciondos/reclutadora/Reclutadora.java
+++ ReclutadoraProject/Reclutadora/src/main/java/programaciondos/reclutadora/Reclutadora.java
@@ -1,8 +1,12 @@
 package programaciondos.reclutadora;
 
+import programaciondos.reclutadora.Presentation.Common.MenuPrincipal;
+
 public class Reclutadora {
 
     public static void main(String[] args) {
+		MenuPrincipal menuPrincipal = new MenuPrincipal();
 		
+		menuPrincipal.mostrarMenu();
     }
 }
