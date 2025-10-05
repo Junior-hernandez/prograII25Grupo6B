f37598ac26306f774eab5f2f62b231f7419c8424
Estiben-Hernandez <jhernandezg92@miumg.edu.gt>
2025-10-04 18:26:49 -0600

    feat(Presentation): Agregar menu principal

---
+++ ReclutadoraProject/Reclutadora/src/main/java/programaciondos/reclutadora/Presentation/Common/MenuPrincipal.java
@@ -0,0 +1,132 @@
+package programaciondos.reclutadora.Presentation.Common;
+
+import java.util.Scanner;
+import programaciondos.reclutadora.Application.DTOs.UsuariosDTOs.UsuarioLoginRequestDTO;
+
+public class MenuPrincipal {
+	private static final Scanner input = new Scanner(System.in); 
+	
+	public void mostrarMenu(){
+			
+		do{
+			try{
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+				
+				System.out.println("\nB I E N V E N I D O");
+
+				System.out.println(" 1. Registrarse \n 2. Iniciar sesion \n 3. Salir");
+
+				System.out.print("\nElije una opcion: ");
+				var option = Integer.parseInt(input.next());
+
+				switch(option){
+					case 1 -> registrar();
+					case 2 -> {
+						logIn();
+						return;
+					}
+					case 3 -> {return;}
+					default -> {
+						System.out.println("Ingresa una opcion entre 1 y 3");
+						input.next();
+					}
+				}
+				
+			}catch(NumberFormatException ex){
+				System.out.println("Debe ingresar un numero entero");
+			}
+			catch(Exception ex){
+				System.out.println("Algo salio mal");
+				input.next();
+			}
+		}while(true);
+	}
+	
+	public static void registrar(){
+		do{
+			try{
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+				System.out.println("\nR E G I S T R A R S E");
+				System.out.println("Que tipo de perfil quieres? \n 1. Postulante  |  2. Empresa");
+				System.out.println("3. Salir");
+				var option = Integer.parseInt(input.next());
+				switch(option){
+					case 1 -> {
+						registrarPostulante();
+						return;
+					}
+					case 2 ->{
+						registrarEmpresa();
+						return;
+					}
+					case 3 ->{return;}
+					default->{
+						System.out.println("Ingresa una opcion entre 1 y 3");
+						input.next();
+					}
+				}
+				
+			}catch(NumberFormatException ex){
+				System.out.println("Debe ingresar un numero entero");
+				input.next();
+			}catch(Exception ex){
+				System.out.println("Algo salió mal intenta de nuevo");
+				input.next();
+			}
+			
+		}while(true);
+	}
+	
+	
+	public static void registrarEmpresa(){
+		try{
+			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			System.out.println("PU PI PU PI.-..--..... \nListo! ya estas registrado como empresa :D");
+			input.next();
+			
+		}catch(Exception ex){
+			
+		}
+	}
+	
+	public static void registrarPostulante(){
+		try{
+			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			System.out.println("PU PI PU PI.-..--..... \nListo! ya estas registrado como postulante :D");
+			input.next();
+			
+		}catch(Exception ex){
+			
+		}
+	}
+	
+	
+	public static void logIn(){
+		do{
+			try{
+				input.nextLine();
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+				
+				System.out.println("I N I C I O   D E   S E S I O N");
+
+				System.out.println("Ingresa tu correo: ");
+				var email = input.nextLine();
+
+				System.out.println("Ingresa tu contrasenia: "); 
+				var contrasenia = input.nextLine();
+
+				System.out.println("Iniciando sesion...");
+
+				var usuarioLogin = new UsuarioLoginRequestDTO(email, contrasenia);
+				
+				
+				// Aqui se va a llamar al AuthService xd
+				
+			}catch(Exception ex){
+				System.out.println("Algo salio mal :c  vuelve a intentar");
+				input.next();
+			}
+		}while(true);
+		
+	}
+}
