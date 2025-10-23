26d2ceddcc2719ae4a2cd66a315956a8158b3fc8
Junior-Hernandez <jhernandezg92@miumg.edu.gt>
2025-10-22 20:19:11 -0600

    feat(Frontend): Agregar página para agregar nuevos postulantes
    También se corrigió la apariencia de las demás paginas

--- 3_Entregable/Frontend/js/postulante.js
+++ 3_Entregable/Frontend/js/postulante.js
@@ -1,4 +1,5 @@
 const tableBody = document.getElementById("table-body");
+document.getElementById("btn-add").addEventListener("click", () => { window.location.href = "postulanteAdd.html" });
 
 const url = "../src/mockData/postulantes.json"; // Esta URL será reemplazada el el endpoint de la API real
 
@@ -13,18 +14,43 @@ fetch(url)
         
         // creamos las celdas de la fila
         const celdaId = document.createElement("td");
-        const celdaNombre = document.createElement("td");
-        const celdaBiografia = document.createElement("td");
-        
-        // llenamos las celdas con los datos del postulante de la iteración
+        celdaId.classList.add("td-id");
         celdaId.textContent = postulante.idPostulante;
+
+        const celdaNombre = document.createElement("td");
         celdaNombre.textContent = postulante.nombre;
+
+        const celdaBiografia = document.createElement("td");
         celdaBiografia.textContent = postulante.biografia;
+
+        const celdaAcciones = document.createElement("td");
+        celdaAcciones.classList.add("td-acciones")
+        
+        // creamos botones de la celda
+        const btnEditar = document.createElement("button");
+        btnEditar.classList.add("btn-editar");
+        const iconoEditar = document.createElement("i");
+        iconoEditar.classList.add("fa", "fa-edit")
+        btnEditar.appendChild(iconoEditar);
+        btnEditar.addEventListener("click", () => editarPostulante(postulante.idPostulante));
+        
+        const btnEliminar = document.createElement("button");
+        btnEliminar.classList.add("btn-eliminar");
+        const iconoEliminar = document.createElement("i");
+        iconoEliminar.classList.add("fa", "fa-trash");
+        btnEliminar.appendChild(iconoEliminar);
+        btnEliminar.addEventListener("click", () => eliminarPostulante(postulante.idPostulante));
+        
+
+        // añadir iconos en botones
+        celdaAcciones.appendChild(btnEditar);
+        celdaAcciones.appendChild(btnEliminar);
 
         // añadimos las celdas a la fila (row)
         row.appendChild(celdaId);
         row.appendChild(celdaNombre);
         row.appendChild(celdaBiografia);
+        row.appendChild(celdaAcciones);
 
         // añadimos la fila al cuerpo de la tabla
         tableBody.appendChild(row);
@@ -33,3 +59,13 @@ fetch(url)
     });
 })
 .catch(error => console.error("Hubo un problema", error)); // sino somos infelices
+
+
+// Funciones
+function editarPostulante(id){
+    alert(`Aqui se editaría el postulante con id: ${id}`);
+}
+
+function eliminarPostulante(id){
+    alert(`Aquí se eliminaría el postulante con el id: ${id}`);
+}
---
+++ 3_Entregable/Frontend/js/postulanteAdd.js
@@ -0,0 +1,33 @@
+document.getElementById("form-postulante").addEventListener("submit", async function(e){
+    e.preventDefault(); // evitar recargar la pagina
+
+    // obtener los valores del formulario
+    const nombre = document.getElementById("nombre").value.trim();
+    
+    const biografia = document.getElementById("biografia").value.trim();
+    
+    const postulante = {
+        nombre: nombre,
+        biografia: biografia
+    };
+
+    // consulta fetch
+
+    const url = "mi url";
+    try{
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(postulante)
+        });
+
+        if(!response.ok) throw new Error("Error al consultar la API");
+
+        alert("Postulante creado excitantemente!")
+        window.location.href = "postulante.html";
+        
+    }catch(error){
+        console.log("Hubo un error.", error);
+        alert("Chales, no se pudo guardar el postulante que querías :c")
+    }
+});
