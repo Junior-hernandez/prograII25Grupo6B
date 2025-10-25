d847a7d1cc1c9e989922ec67a9e713046ade782e
Junior-Hernandez <jhernandezg92@miumg.edu.gt>
2025-10-24 22:57:35 -0600

    hotFix(Backend): Arreglando conflictos de merge

--- 3_Entregable/BackEnd/src/main/java/gt/edu/umg/Entities/Empresa.java
+++ 3_Entregable/BackEnd/src/main/java/gt/edu/umg/Entities/Empresa.java
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-/*
-=======
-/*-
->>>>>>> 58d12e7f9c23b124e6c788d20c048597fd0893f7
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gt.edu.umg.Entities;
 
 import java.io.Serializable;
@@ -16,16 +8,6 @@ import javax.persistence.Table;
 
 /**
  *
-<<<<<<< HEAD
- * @author Deyvid Hernández
- */
-@Entity
-@Table (name="Empresas")
-public class Empresa implements Serializable {
-	@Id
-	@Column(name="IdEmpresa")
-	private int IdEmpresa;
-=======
  * @author Deyvid Hernandez
  */
 
@@ -36,7 +18,6 @@ public class Empresa  implements Serializable {
 	@Id
 	@Column (name="IdEmpresa")
 	private  int Id;
->>>>>>> 58d12e7f9c23b124e6c788d20c048597fd0893f7
 	
 	@Column (name="Nombre")
 	private String Nombre;
@@ -47,21 +28,12 @@ public class Empresa  implements Serializable {
 	@Column (name="Direccion")
 	private String Direccion;
 
-<<<<<<< HEAD
-	public int getIdEmpresa() {
-		return IdEmpresa;
-	}
-
-	public void setIdEmpresa(int IdEmpresa) {
-		this.IdEmpresa = IdEmpresa;
-=======
 	public int getId() {
 		return Id;
 	}
 
 	public void setId(int Id) {
 		this.Id = Id;
->>>>>>> 58d12e7f9c23b124e6c788d20c048597fd0893f7
 	}
 
 	public String getNombre() {
@@ -86,18 +58,6 @@ public class Empresa  implements Serializable {
 
 	public void setDireccion(String Direccion) {
 		this.Direccion = Direccion;
-<<<<<<< HEAD
-	}
-
-	@Override
-	public String toString() {
-		return "Empresa{" + "IdEmpresa=" + IdEmpresa + ", Nombre=" + Nombre + ", Biografia=" + Biografia + ", Direccion=" + Direccion + '}';
-	}
-	
-	
-	
-	
-=======
 	}	
 
 	@Override
@@ -105,5 +65,4 @@ public class Empresa  implements Serializable {
 		return "Empresa{" + "Id=" + Id + ", Nombre=" + Nombre + ", Biografia=" + Biografia + ", Direccion=" + Direccion + '}';
 	}
 	
->>>>>>> 58d12e7f9c23b124e6c788d20c048597fd0893f7
 }
